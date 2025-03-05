1750bfba7fd6b7ce6eaaab3e85143b17ff6d3a28
Raman Sehgal <sc.ramansehgal@gmail.com>
2025-03-05 22:57:01 +0530

    Added additional detector planes and known scatterer slab for momentum estimation

--- src/Global.cpp
+++ src/Global.cpp
@@ -3,22 +3,21 @@
 **	2025-03-05
 **	username : rsehgal
 */
-#include "Global.h"
 #include "G4SystemOfUnits.hh"
+#include "Global.h"
 double gPlaneSizeX = 112.4 * cm;
 double gPlaneSizeZ = 112.4 * cm;
-double gPlaneSize  = 112.4 * cm;
-int gNumOfStrips   = 100;
-int gNumOfStripsX  = 100;
-int gNumOfStripsZ  = 100;
+double gPlaneSize = 112.4 * cm;
+int gNumOfStrips = 100;
+int gNumOfStripsX = 100;
+int gNumOfStripsZ = 100;
 
-int gNumOfMasks             = 10;
-std::vector<double> yPosVec = {-80. * cm, -50 * cm, 50 * cm, 80. * cm};
+int gNumOfMasks = 10;
+std::vector<double> yPosVec = {-130 * cm, -100 * cm, -80. * cm, -50 * cm, 50 * cm, 80. * cm};
 
 std::map<int, G4ThreeVector> hitPointVec;
 
-void InitializeHitPointVec()
-{
+void InitializeHitPointVec() {
   for (unsigned int i = 0; i < yPosVec.size(); i++) {
     hitPointVec[i] = G4ThreeVector(-50000., yPosVec[i], -50000.);
   }
--- src/SingleTelescope.cpp
+++ src/SingleTelescope.cpp
@@ -3,45 +3,50 @@
 **	2025-01-12
 **	username : rsehgal
 */
-#include "Geometry.h"
-#include <G4LogicalVolume.hh>
-#include <iostream>
-#include <G4Box.hh>
-#include <G4SystemOfUnits.hh>
 #include "G4PVPlacement.hh"
+#include "Geometry.h"
+#include "Global.h"
+#include <G4Box.hh>
+#include <G4LogicalVolume.hh>
 #include <G4RotationMatrix.hh>
 #include <G4SystemOfUnits.hh>
-#include "Global.h"
+#include <iostream>
 SingleTelescope::SingleTelescope() {}
 
 SingleTelescope::~SingleTelescope() {}
 
-SingleTelescope::SingleTelescope(G4String name, unsigned int numOfScintillators, unsigned short numOfGroups)
-{
+SingleTelescope::SingleTelescope(G4String name, unsigned int numOfScintillators, unsigned short numOfGroups) {
 
-  //std::vector<double> yPosVec           = {-80. * cm, -50 * cm, 50 * cm, 80. * cm};
-  //std::vector<double> yPosVec           = {-275. * cm, -175 * cm, 175 * cm, 275. * cm};
-  //std::vector<double> yPosVec           = {0. * cm};
+  // std::vector<double> yPosVec           = {-80. * cm, -50 * cm, 50 * cm, 80. * cm};
+  // std::vector<double> yPosVec           = {-275. * cm, -175 * cm, 175 * cm, 275. * cm};
+  // std::vector<double> yPosVec           = {0. * cm};
   G4LogicalVolume *logicalTomoLayer = (new TomoLayer("TomoLayer", 100, 10))->GetLogicalVolume();
 
-  G4Box *solid                 = static_cast<G4Box *>(logicalTomoLayer->GetSolid());
+  G4Box *solid = static_cast<G4Box *>(logicalTomoLayer->GetSolid());
 
-  double envelopHalfX = solid->GetXHalfLength() + 1; //0.5
-  double envelopHalfY = yPosVec[yPosVec.size()-1]+ 2 * solid->GetYHalfLength() + 1;
-  double envelopHalfZ = solid->GetZHalfLength() + 1;//0.5;
+  double envelopHalfX = solid->GetXHalfLength() + 1; // 0.5
+  double yrange = std::fabs(yPosVec[0]) > std::fabs(yPosVec[yPosVec.size() - 1])
+                      ? std::fabs(yPosVec[0])
+                      : std::fabs(yPosVec[yPosVec.size() - 1]);
+  // double envelopHalfY = yPosVec[yPosVec.size()-1]+ 2 * solid->GetYHalfLength() + 1;
+  double envelopHalfY = yrange + 2 * solid->GetYHalfLength() + 1;
+  double envelopHalfZ = solid->GetZHalfLength() + 1; // 0.5;
   double envelopHalfXZ = envelopHalfX > envelopHalfZ ? envelopHalfX : envelopHalfZ;
 
   bool checkOverlaps = true;
-  fLogicalVolume     = (new Box(name, envelopHalfXZ, envelopHalfY, envelopHalfXZ))->GetLogicalVolume();
-
+  fLogicalVolume = (new Box(name, envelopHalfXZ, envelopHalfY, envelopHalfXZ))->GetLogicalVolume();
 
   for (unsigned int i = 0; i < yPosVec.size(); i++) {
-    new G4PVPlacement(0, G4ThreeVector(0, yPosVec[i], 0), logicalTomoLayer, "PhysicalTomoLayer", fLogicalVolume, false, i,
-                      checkOverlaps);
+    new G4PVPlacement(0, G4ThreeVector(0, yPosVec[i], 0), logicalTomoLayer, "PhysicalTomoLayer", fLogicalVolume, false,
+                      i, checkOverlaps);
   }
 
-  G4LogicalVolume *logicalScatterer     = (new Box("Scatterer", 10*cm, 10*cm, 10*cm, "G4_Pb"))->GetLogicalVolume();
-  new G4PVPlacement(0, G4ThreeVector(0,0.,0), logicalScatterer, "PhysicalScatterer", fLogicalVolume, false, 0,
-                      checkOverlaps);
-  
+  G4LogicalVolume *logicalScatterer = (new Box("Scatterer", 10 * cm, 10 * cm, 10 * cm, "G4_Pb"))->GetLogicalVolume();
+  new G4PVPlacement(0, G4ThreeVector(0, 0., 0), logicalScatterer, "PhysicalScatterer", fLogicalVolume, false, 0,
+                    checkOverlaps);
+
+  G4LogicalVolume *logicalMomentumScatterer =
+      (new Box("MomentumDetectionScattererSlab", 50 * cm, 10 * cm, 50 * cm, "G4_Pb"))->GetLogicalVolume();
+  new G4PVPlacement(0, G4ThreeVector(0, -115. * cm, 0), logicalMomentumScatterer, "PhysicalMomentumScatterer",
+                    fLogicalVolume, false, 0, checkOverlaps);
 }
